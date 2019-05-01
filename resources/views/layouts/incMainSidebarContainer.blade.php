675c7e53c91d74a00115164dbf4935bca3bd5961
dalorkee <dalorkee@gmail.com>
2019-05-01 13:31:51 +0700

    edit css link

--- resources/views/layouts/incMainSidebarContainer.blade.php
+++ resources/views/layouts/incMainSidebarContainer.blade.php
@@ -2,7 +2,7 @@
 	<aside class="main-sidebar sidebar-dark-primary elevation-4">
 		<!-- Brand Logo -->
 		<a href="#" class="brand-link">
-			{{ Html::image('public/images/small-moph-logo.png', 'alt=DDC Logo', array('class'=>'brand-image img-circle elevation-3', 'style'=>'opacity: .8')) }}
+			{{ Html::image('images/small-moph-logo.png', 'alt=DDC Logo', array('class'=>'brand-image img-circle elevation-3', 'style'=>'opacity: .8')) }}
 			<span class="brand-text font-weight-light">DDC EOC</span>
 		</a>
 		<!-- Sidebar -->
@@ -10,7 +10,7 @@
 			<!-- Sidebar user panel (optional) -->
 			<div class="user-panel mt-3 pb-3 mb-3 d-flex">
 				<div class="image">
-					{{ Html::image('public/images/default-avatar.png', 'alt=avatar', array('class'=>'img-circle elevation-2')) }}
+					{{ Html::image('images/default-avatar.png', 'alt=avatar', array('class'=>'img-circle elevation-2')) }}
 				</div>
 				<div class="info">
 					<a href="#" class="d-block">BOE PJ</a>
